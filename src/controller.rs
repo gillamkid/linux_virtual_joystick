d00a017fee11b81ac9b5038ff177471aff1cc5a3
Sam Gillam <gillamkid@gmail.com>
2025-08-21 16:27:05 -0600

    use axes IDs that work on ubuntu 22 and ubuntu 24 sdl2

--- src/controller.rs
+++ src/controller.rs
@@ -13,8 +13,8 @@ const SLIDER_AXES: [(AbsoluteAxisType, &str, i32, i32); 8] = [
     (AbsoluteAxisType::ABS_Y, "Left Joystick Y", LEFT_START, 155),
     (AbsoluteAxisType::ABS_RX, "Right Joystick X", RIGHT_START, 190),
     (AbsoluteAxisType::ABS_RY, "Right Joystick Y", RIGHT_START + 110, 155),
-    (AbsoluteAxisType::ABS_HAT1X, "Left Hat X", LEFT_START + 40, 340),
-    (AbsoluteAxisType::ABS_HAT1Y, "Left Hat Y", LEFT_START + 10, 305),
+    (AbsoluteAxisType::ABS_WHEEL, "Left Hat X", LEFT_START + 40, 340),
+    (AbsoluteAxisType::ABS_GAS, "Left Hat Y", LEFT_START + 10, 305),
     (AbsoluteAxisType::ABS_THROTTLE, "Left Trigger", LEFT_START + 160, 5),
     (AbsoluteAxisType::ABS_RUDDER, "Right Trigger", RIGHT_START - 140, 5),
 ];
